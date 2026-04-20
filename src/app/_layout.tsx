bfa8d9b41bbe245626608691ff9a0c89bcc1b0d1
rozer <shubhamsinha07084@gmail.com>
2026-04-21 00:52:02 +0530

    updated layout - (tabs)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,15 @@
-import { colors } from "@/styles/global";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack
     screenOptions={{
       // headerTitle: 'Nutriledger',
-      // headerShown: false,
-      headerStyle: {backgroundColor: colors.header},
-      headerTintColor: '#fff',
+      headerShown: false,
+      // headerStyle: {backgroundColor: colors.header},
+      // headerTintColor: '#fff',
 
     }}
   >
-    <Stack.Screen name='index' options={{headerShown: false, title: 'Home'}} />
-    <Stack.Screen name='meals' options={{ title: 'Meals'}} />
-    <Stack.Screen name='add-meal' options={{ title: 'Add Meal'}} />
+    <Stack.Screen name='{tabs}' />
   </Stack>;
 }
