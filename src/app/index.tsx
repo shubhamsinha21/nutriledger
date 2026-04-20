79a7de072c194734faff0d40f1839a182fb6c0ad
rozer <shubhamsinha07084@gmail.com>
2026-04-21 00:05:45 +0530

    Navigating to meal screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,16 @@
 import HomeHeader from "@/components/HomeHeader";
 import { globalStyles } from "@/styles/global";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { Link } from "expo-router";
+import { ScrollView, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>Nutriledger</Text>
-      <Text style={styles.date}>Monday, March 16</Text>
       <HomeHeader />
+      <Link href='/meals' style={{fontSize: 18, color: '#007bff'}}>
+        Go to Meals
+      </Link>
     </ScrollView>
   )
 }
-
-const styles = StyleSheet.create({
-  date: {
-    fontSize: 14,
-    color: '#a0a0b0',
-    marginTop: 4,
-    marginBottom: 30,
-  }
-})
