25619a07fc04d6dec190189bf4b166f53090ea0d
rozer <shubhamsinha07084@gmail.com>
2026-04-21 00:25:03 +0530

    Added header to meal screen and created stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,18 @@
+import { colors } from "@/styles/global";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack
     screenOptions={{
       // headerTitle: 'Nutriledger',
-      headerShown: false,
+      // headerShown: false,
+      headerStyle: {backgroundColor: colors.header},
+      headerTintColor: '#fff',
+
     }}
   >
-    <Stack.Screen name='index' />
-    <Stack.Screen name='meals' />
+    <Stack.Screen name='index' options={{headerShown: false, title: 'Home'}} />
+    <Stack.Screen name='meals' options={{ title: 'Meals'}} />
+    <Stack.Screen name='add-meal' options={{ title: 'Add Meal'}} />
   </Stack>;
 }
