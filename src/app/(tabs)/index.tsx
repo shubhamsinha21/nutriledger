07bcb3f938c0d7133939d1d1c1b4d1e7c88d2351
rozer <shubhamsinha07084@gmail.com>
2026-04-21 11:28:32 +0530

    import NutritionGrid

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import HomeHeader from "@/components/HomeHeader";
+import NutritionGrid from "@/components/NutritionGrid";
 import { globalStyles } from "@/styles/global";
-import { ScrollView, Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <ScrollView style={globalStyles.container}>
+    <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Nutriledger</Text>
       <HomeHeader />
-    </ScrollView>
+      <NutritionGrid/>
+    </View>
   )
 }
